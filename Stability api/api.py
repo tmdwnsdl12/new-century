3682d80f2f96138ad5bbf2494e13aebbed6daecd
tmdwnsdl12 <133727654+tmdwnsdl12@users.noreply.github.com>
2023-07-06 13:33:21 +0900

    ML stabilituy api  - app

---
+++ Stability api/api.py
@@ -0,0 +1,32 @@
+from auth_token import auth_token
+from fastapi import FastAPI, Response
+from fastapi.middleware.cors import CORSMiddleware
+import torch
+from torch import autocast
+from diffusers import StableDiffusionPipeline
+from io import BytesIO
+import base64
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+device= "cuda"
+model_id = "runwayml/stable-diffusion-v1-5"
+pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
+pipe.to(device)
+
+@app.get("/")
+def generate(prompt: str):
+    with autocast(device):
+        image = pipe(prompt, guidance_scale=8.5).image[0]
+        
+    image.save("testimage.png")
+    
+    return {"out":"hello world"}
